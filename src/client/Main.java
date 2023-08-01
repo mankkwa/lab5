2b2aa025768db1578c2ca899381152f7c772a53f
gstru <93343406+mankkwa@users.noreply.github.com>
2023-08-01 13:55:41 +0300

    1 (я поняла как выводить шок)

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,7 +1,13 @@
 package client;
 
+import client.handlers.ConsoleInputHandler;
+import client.handlers.InputHandler;
+
 public class Main {
     public static void main(String[] args) {
+
+        ConsoleInputHandler cih = new ConsoleInputHandler();
+        System.out.println(cih.readInput());
 
     }
 }
